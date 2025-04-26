a650995dbe72af7753c55276632ca5d1265beeb6
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-04-26 19:53:44 +0300

    project started

---
+++ src/test/java/ksis/demo/Lab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ksis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
